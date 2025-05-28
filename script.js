1e231dbf3bb67c1cdbb19b0deddae5b2366b10e2
Marco Scazzari <marco.scazzari@lefonti.com>
2025-05-28 11:34:24 +0200

    Aggiunta referenza + Navbar statica + JS

--- script.js
+++ script.js
@@ -27,3 +27,13 @@ function typeEffect() {
 }
 
 typeEffect();
+
+  const nav = document.querySelector('nav');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
